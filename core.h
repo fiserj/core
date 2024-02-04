769f3e91844c3dd1b45af8790c44522a53bcce15
Jakub Fiser <fiser87@gmail.com>
2024-02-04 22:10:30 +0000

    Corrected signed int typedefs

--- core.h
+++ core.h
@@ -117,10 +117,10 @@ void log(const char* _kind, detail::Fmt _fmt, ...);
 using Index = ptrdiff_t;
 using Size  = Index;
 
-using i8  = uint8_t;
-using i16 = uint16_t;
-using i32 = uint32_t;
-using i64 = uint64_t;
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 
 using u8  = uint8_t;
 using u16 = uint16_t;
